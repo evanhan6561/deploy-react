bc25d1b05e2837ae43f42c3c15f4512647885c37
evanhan6561 <54378519+evanhan6561@users.noreply.github.com>
2020-04-23 10:13:07 -0500

    Nani tf

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          B R U H M O M E N T
         </p>
         <a
           className="App-link"
