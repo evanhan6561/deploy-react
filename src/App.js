9fba909234eef2d6ddbc27359b2e66edaa0103ad
evanhan6561 <54378519+evanhan6561@users.noreply.github.com>
2020-04-23 10:25:08 -0500

    nani

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          B R U H M O M E N T
+          Nani tf
         </p>
         <a
           className="App-link"
